80cef9e8aac6436ab36c10711efb5ea094388919
Gianluca Capraro <gmcapra@umich.edu>
2019-07-12 13:53:16 -0400

    Update gan_project_mnist_generator.py

--- Generative Adversarial Networks/gan_project_mnist_generator.py
+++ Generative Adversarial Networks/gan_project_mnist_generator.py
@@ -22,8 +22,8 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("../03-Convolutional-Neural-Networks/MNIST_data/",one_hot=True)
 
-print("Showing example MNIST Handwritten Number...")
-plt.imshow(mnist.train.images[5].reshape(28,28),cmap='Greys')
+print("\nShowing example MNIST Handwritten Number...")
+plt.imshow(mnist.train.images[9].reshape(28,28),cmap='Greys')
 plt.show()
 print('\n')
 
